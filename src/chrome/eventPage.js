d90965c99b81cd5ed1a60b31842377394c11c167
maxim199840 <maxim-berezovskiy@mail.ru>
2018-01-24 16:11:01 +0200

    broke everything

--- src/chrome/eventPage.js
+++ src/chrome/eventPage.js
@@ -1,6 +1,58 @@
 import {storageController} from './storageController';
 import {db} from '../firbaseController';
+import {Messenger} from './Messenger';
 
+let lobbyMessenger = new Messenger('lobby');
+let videoSyncMessenger = new Messenger('video');
+let connectedLobbyRef = {};
+lobbyMessenger.addListener(message => console.log(message));
+(function() {
+  videoSyncMessenger.addListener(videoMessagesListener);
+  storageController.onChange = onStorageChange;
+  storageController.getAsyncStorage('lobbyId').then(data => {
+    if (data.lobbyId === null) return;
+    connectFirebaseLobby(data.lobbyId);
+
+  });
+})();
+
+function connectFirebaseLobby(id) {
+  connectedLobbyRef =  db.ref(`lobbies/${id}`);
+  connectedLobbyRef.child('/link').on('value', link => {
+    console.log(link.val());
+    console.log(lobbyMessenger);
+    // lobbyMessenger.send({link: link.val()});
+  })
+}
+
+function onStorageChange(changeObj) {
+  if (changeObj.lobbyId !== undefined) {
+    onLobbyChanged(changeObj.lobbyId);
+  }
+}
+
+function onLobbyChanged(newId) {
+  connectFirebaseLobby(newId);
+}
+
+
+
+function videoMessagesListener(message){
+  if(message.playing){
+    connectedLobbyRef.child('isPlaying').set(message.playing);
+  }
+  else if(message.updateTime){
+    connectedLobbyRef.update({updateTime: message.updateTime});
+  }
+}
+
+function checkURL(tab) {
+  let url = new URL(tab.url);
+  return ((url.hostname === connectedLobby.link.domain) &&
+      (url.searchParams.get('v') === connectedLobby.link.id));
+}
+
+/*
 let connectedLobby = {};
 let executed = false;
 storageController.setAsyncStorage({executed: false});
@@ -34,11 +86,7 @@ function executeScriptToYoutube() {
   });
 }
 
-function checkURL(tab) {
-  let url = new URL(tab.url);
-  return ((url.hostname === connectedLobby.link.domain) &&
-      (url.searchParams.get('v') === connectedLobby.link.id));
-}
+
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeObj, updatedTab) {
   console.log(changeObj);
@@ -48,4 +96,4 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeObj, updatedTab) {
           {file: './youTube.js'});
     }
   }
-});
+});*/
