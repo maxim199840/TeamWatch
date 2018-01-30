f60b5a246c1aed09f147a640f8f29703a2f07dc3
RedGeekPanda <redgeekpanda@gmail.com>
2018-01-30 16:26:41 +0200

    add disconnect function in lobbies history overview and refactor injection

--- src/scripts/injection.js
+++ src/scripts/injection.js
@@ -3,66 +3,74 @@ import {LINK_WITH_LOBBY, VIDEO_CONTROL} from '../messageTypes';
 
 (function() {
 
-  browser.runtime.onMessage.addListener(onMessage);
+  browser.runtime.onMessage.addListener(onSyncListener);
 
+  let videoIdentity;
+  let lobbyId;
   let video;
 
-  function onMessage({type, payload}, sender, responseCallback) {
-    switch (type) {
-      case LINK_WITH_LOBBY: {
-        const isTabMatched = checkVideoIdentityMatch(payload.videoIdentity);
-        if (!isTabMatched) {
-          responseCallback(false);
-          return;
-        }
-        responseCallback(true);
+  function onSyncListener({type, payload}, sender, responseCallback) {
+    if (type !== LINK_WITH_LOBBY) return;
 
-        video = document.
-            getElementsByClassName('video-stream html5-main-video')[0];
-        const port = browser.runtime.connect();
-        port.onMessage.addListener(onMessage);
-        port.postMessage({
-          type: LINK_WITH_LOBBY,
-          payload: {
-            lobbyId: payload.lobbyId,
-          },
-        });
+    const isTabMatched = checkVideoIdentityMatch(payload.videoIdentity);
+    if (!isTabMatched) {
+      responseCallback(false);
+      return;
+    }
+    responseCallback(true);
 
-        video.onplay = () => port.postMessage({
-          type: VIDEO_CONTROL,
-          payload: {
-            isPlaying: true,
-          },
-        });
-        video.onpause = () => port.postMessage({
-          type: VIDEO_CONTROL,
-          payload: {
-            isPlaying: false,
-          },
-        });
-        video.onseeked = () => port.postMessage({
-          type: VIDEO_CONTROL,
-          payload: {
-            time: video.currentTime,
-          },
-        });
+    browser.runtime.onMessage.removeListener(onSyncListener);
 
-        break;
+    videoIdentity = payload.videoIdentity;
+    lobbyId = payload.lobbyId;
+
+    console.log(videoIdentity, lobbyId);
+
+    video = document.getElementsByClassName('video-stream html5-main-video')[0];
+
+    const port = browser.runtime.connect();
+    port.onMessage.addListener(onVideoControlListener);
+    port.postMessage({
+      type: LINK_WITH_LOBBY,
+      payload: {
+        lobbyId: payload.lobbyId,
+      },
+    });
+
+    video.onplay = () => port.postMessage({
+      type: VIDEO_CONTROL,
+      payload: {
+        isPlaying: true,
+      },
+    });
+    video.onpause = () => port.postMessage({
+      type: VIDEO_CONTROL,
+      payload: {
+        isPlaying: false,
+      },
+    });
+    video.onseeked = () => port.postMessage({
+      type: VIDEO_CONTROL,
+      payload: {
+        isPlaying: false,
+        time: video.currentTime,
+      },
+    });
+  }
+
+  function onVideoControlListener({type, payload}) {
+    if (type !== VIDEO_CONTROL) return;
+
+    const {isPlaying, time} = payload;
+    if (typeof(isPlaying) === 'boolean') {
+      if (isPlaying) {
+        video.play();
+      } else {
+        video.pause();
       }
-      case VIDEO_CONTROL: {
-        const {isPlaying, time} = payload;
-        if (typeof(isPlaying) === 'boolean') {
-          if (isPlaying) {
-            video.play();
-          } else {
-            video.pause();
-          }
-        }
-        if (typeof time === 'number') {
-          video.currentTime = time;
-        }
-        break;
-      }
+    }
+    if (typeof time === 'number') {
+      video.currentTime = time;
     }
   }
 
